5993f9b658971686189c52e3a140e07eedff9598
Stars-Hiker <starshiker@proton.me>
2024-06-04 20:45:47 +0200

    2nd commit

--- partitioning.sh
+++ partitioning.sh
@@ -1,5 +1,10 @@
 #!/bin/env bash
 
+#humanSize() {
+#    hr=$(echo "scale=5; $1" | bc)
+#    echo $hr
+#}
+
 # Show disk on computer
 showDisk() {
     echo "--- YOUR DISKS ---"
@@ -16,25 +21,32 @@ chooseDisk() {
     echo
     # Get the disk size
     diskSize=$(lsblk -b | grep -w "$diskName" | awk '{ print $4 }')
-    diskSize=$(echo "scale=2; $diskSize / (1024^3)" | bc)  # Convert size from bytes to gigabytes
+    diskSize=$(echo "scale=20; $diskSize / (1024^3)" | bc)  # Convert size from bytes to gigabytes
     part
 }
 
 part() {
+    #fdiskSize=$(humanSize $diskSize)
     echo "--- DISK $diskName IS $diskSize G ---"
     echo
     echo "-- How many OS would you like to install? --"
     echo
     read -r osNumber
-    bootSize=$(echo "scale=4; $osNumber * 0.55" | bc)
+    bootSize1=$((1024 * 1024 * 550))
+    bootSizeInGB=$(echo "scale=20; $bootSize1 / (1024^3)" | bc)
+    bootSize=$(echo "scale=20; $osNumber * $bootSizeInGB" | bc)
     echo
+    #fbootSize=$(humanSize $bootSize)
     echo "-- BOOT SIZE WILL BE $bootSize G --"
     echo
     echo "-- CHOOSE SIZE FOR THE MAIN OS IN GIGABYTES --"
     read -r mainOsSize
+    #mainSizeByte=$(( 1024 * 1024 * 1024 * $mainOsSize ))
+    #mainSizeGB=$( echo "scale=20; $mainSizeByte / (1024^3)" )
     echo
-    restOsSize=$(echo "scale=4; $diskSize - $mainOsSize - $bootSize" | bc)
+    restOsSize=$(echo "scale=20; $diskSize - $mainOsSize - $bootSize" | bc)
     echo
+    #frestOsSize=$(humanSize $restOsSize)
     echo "-- STILL $restOsSize G TO BE USED --"
     echo
     partitionSize
@@ -42,13 +54,14 @@ part() {
 
 partitionSize() {
     restOsNumber=$((osNumber - 1))
-    partsize=$(echo "scale=4; $restOsSize / $restOsNumber" | bc)
+    partsize=$(echo "scale=20; $restOsSize / $restOsNumber" | bc)
+    #fpartsize=$(humanSize $partsize)
     echo "-- THE OTHER PARTITION WILL BE $partsize G --"
     totalPartitionNumber=$((osNumber + 1))
     echo
     echo "-- $totalPartitionNumber PARTITIONS WILL BE MADE --"
     echo "-- BOOT sda1 WILL BE $bootSize G --"
-    echo "-- MAIN PARTITION WILL BE $mainOsSize G --"
+    echo "-- MAIN PARTITION sda2 WILL BE $mainOsSize G --"
     echo "-- $restOsNumber OTHER PARTITIONS OF $partsize G WILL BE MADE --"
     echo
 }
