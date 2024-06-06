9c48f6df7efe06deb7c67b6b4adca0e2a5b485e4
Stars-Hiker <starshiker@proton.me>
2024-06-06 10:39:58 +0200

    3rd commit

---
+++ archInstall-MainOs-gpt.sh
@@ -0,0 +1,338 @@
+#!/bin/env bash
+
+# Source partition script
+source partitionDisk3Gpt.sh
+
+# Set default console keyboard
+loadkeys fr-latin1
+
+# Function to set font size
+set_font_size() {
+    echo "--- FOR BIGGER FONT SIZE TYPE 'yes'"
+    echo "--- OTHERWISE JUST PRESS 'RETURN'"
+    read -r answer
+    if [ "$answer" = "yes" ]; then
+        setfont ter-124b
+    else
+        echo "No changes have been made"
+    fi
+}
+
+# Start the script
+set_font_size
+
+# Update system clock
+timedatectl
+
+# Function to edit Pacman configuration
+editPac() {
+    local pFile=/etc/pacman.conf
+    sed -i 's/#Color/Color/' $pFile
+    sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 10/' $pFile
+    sed -i 's/#\[multilib\]/\[multilib\]/' $pFile
+    sed -i '91s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/' $pFile
+}
+
+editPac
+
+# Function for disk partitioning with gdisk
+gdisk_partition() {
+    gdisk /dev/$disk <<EOF
+o
+Y
+n
+
+
++1G
+ef00
+c
+BOOT
+n
+
+
+
+
+c
+2
+ROOT
+w
+Y
+EOF
+}
+
+# Function for disk partitioning with fdisk
+fdisk_partition() {
+fdisk /dev/$disk <<EOF
+g
+n
+1
+
++1G
+t
+1
+n
+
+
+
+x
+n
+1
+BOOT
+n
+2
+ROOT
+r
+w
+EOF
+}
+
+# Show available disks and choose one
+showChooseDisk() {
+    echo "--- THESE ARE YOUR DISKS ---"
+    fdisk -l | grep "Disk" | grep "/dev/"
+    echo "--- ENTER THE LAST 3 CHARACTERS OF THE DISK YOU WANNA USE"
+    read -r disk
+}
+
+# Partition function
+partition() {
+    echo
+    echo "--- FOR > CUSTOM PARTITIONING TYPE 'c'"
+    echo "--- FOR > AUTOMATED PARTITIONING TYPE 'a'"
+    echo "--- FOR > SCRIPTED PARTITIONING TYPE 's'---"
+    echo
+    read -r partAnswer
+
+    if [ "$partAnswer" = "a" ]; then
+        showChooseDisk
+        echo "--- TYPE 'g' TO USE gdisk"
+        echo "OR"
+        echo "--- TYPE 'f' TO USE fdisk"
+        read -r gORf
+        if [ "$gORf" = "g" ]; then
+            gdisk_partition
+        elif [ "$gORf" = "f" ]; then
+            fdisk_partition
+        else
+            partition
+        fi
+    elif [ "$partAnswer" = "c" ]; then
+        showChooseDisk
+        echo "--- MANUAL PARTITIONING WITH cfdisk, gdisk, or fdisk (cf/g/f)"
+        read -r custom
+        case $custom in
+            cf) cfdisk /dev/"$disk" ;;
+            g) gdisk /dev/"$disk" ;;
+            f) fdisk /dev/"$disk" ;;
+            *) echo "--- WRONG ENTRY: TRY AGAIN"; partition ;;
+        esac
+    elif [ "$partAnswer" = "s" ]; then
+        showDisk
+    else
+        echo "--- WRONG ENTRY: TRY AGAIN"
+        partition
+    fi
+}
+
+partition
+
+# Format partitions
+mkfs.vfat -F 32 -n BOOT /dev/"$disk"1
+mkfs.btrfs -f -L ROOT /dev/"$disk"2
+
+# Create btrfs subvolumes
+mount /dev/"$disk"2 /mnt
+cd /mnt
+btrfs subvolume create @
+btrfs subvolume create @home
+cd
+umount /mnt
+
+# Mount subvolumes
+mount -o rw,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2,subvol=@ /dev/"$disk"2 /mnt
+mkdir -p /mnt/{boot,home}
+mount -o rw,noatime,compress=zstd:3,ssd,discard=async,space_cache=v2,subvol=@home /dev/"$disk"2 /mnt/home
+mount /dev/"$disk"1 /mnt/boot
+
+# Refresh package databases and install base system
+reflector --country France,Germany --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy
+pacstrap -K /mnt base base-devel linux linux-firmware git nano neovim openssh reflector networkmanager iwd ufw rsync amd-ucode alacritty kitty zsh libvirt
+
+# Generate fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Get the PARTUUID of ROOT
+diskid=$(blkid | grep "$disk"2 | grep -o 'PARTUUID="[^"]*"' | sed 's/PARTUUID="//;s/"//')
+
+# Create new user
+promptUser() {
+    echo "-- ENTER NEW USER NAME --"
+    read -r userName
+    if [[ ! $userName =~ ^[a-zA-Z][a-zA-Z0-9_-]*$ || ${#userName} -gt 32 ]];then
+        echo "-- NOT A VALID USER NAME --"
+        echo "-- USER NAME MUST START WITH A LETTER AND CAN ONLY CONSIST OF: --"
+        echo "-- LETTERS, DIGITS, HYPHENS '-' AND UNDERSCORES '_' AND --"
+        echo "-- CAN BE UP TO 32 CHARACTERS --"
+        promptUser
+    else
+        echo "-- VALID USER NAME: $userName --"
+    fi
+}
+promptUser
+
+
+# Prompt user for passwords outside the chroot environment
+promptRootPasswd() {
+echo "--- Enter password for root: "
+read -s ROOTPASS
+echo "--- Confirm password for root: "
+read -s ROOTPASS_CONFIRM
+
+if [ "$ROOTPASS" != "$ROOTPASS_CONFIRM" ]; then
+    echo "Passwords do not match."
+    promptRootPasswd
+else
+    echo "-- PASSWORD MATCH --"
+fi
+}
+promptRootPasswd
+
+promptUserPasswd() {
+echo "--- Enter password for new user $userName: "
+read -s USERPASS
+echo "--- Confirm password for new user $userName: "
+read -s USERPASS_CONFIRM
+
+if [ "$USERPASS" != "$USERPASS_CONFIRM" ]; then
+    echo "Passwords do not match."
+    promptUserPasswd
+else
+    echo "-- PASSWORD MATCH --"
+fi
+}
+promptUserPasswd
+
+# Export passwords as environment variables
+export ROOTPASS
+export USERPASS
+
+
+
+# Password prompt function
+#promptPassword() {
+#    local password1 password2
+#    while true; do
+#        read -s -r -p "--- Enter password for $1: " password1
+#        echo
+#        read -s -r -p "--- Confirm password for $1: " password2
+#        echo
+#        if [ "$password1" == "$password2" ]; then
+#            echo "$password1"
+#            return
+#        else
+#            echo "Passwords do not match. Please try again."
+#        fi
+#    done
+#}
+
+#userPass=$(promptPassword "$userName")
+#rootPass=$(promptPassword "root")
+
+#export USERPASS="$userPass"
+#export ROOTPASS="$rootPass"
+
+# Chroot and configure system
+arch-chroot /mnt << EOF
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc --localtime
+
+reflector --country France,Germany --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syyu
+
+sed -i 's/#Color/Color/' $pFile
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 8/' $pFile
+sed -i 's/#\[multilib\]/\[multilib\]/' $pFile
+sed -i '91s/#Include = \/etc\/pacman.d\/mirrorlist/Include = \/etc\/pacman.d\/mirrorlist/' $pFile
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+
+locale-gen
+
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+echo KEYMAP=fr-latin1 > /etc/vconsole.conf
+echo arch > /etc/hostname
+
+cat <<EOT >> /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   arch.localdomain arch
+EOT
+
+bootctl install
+
+cat <<EOT >> /boot/loader/loader.conf
+default arch-Hyprland.conf
+timeout 3
+console-mode max
+editor 0
+EOT
+
+cat <<EOT >> /boot/loader/entries/arch-Hyprland.conf
+title   Arch Linux Hyprland
+linux   /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux.img
+options root=PARTUUID=$diskid zswap.enabled=0 rootflags=subvol=@ rw rootfstype=btrfs
+EOT
+
+cat <<EOT >> /boot/loader/entries/arch-Hyprland-fallback.conf
+title   Arch Linux Hyprland
+linux   /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux-fallback.img
+options root=PARTUUID=$diskid zswap.enabled=0 rootflags=subvol=@ rw rootfstype=btrfs
+EOT
+
+cat << 'EOT' > /etc/systemd/zram-generator.conf
+[zram0]
+zram-size = ram / 4
+EOT
+
+cat << 'EOT' > /etc/sysctl.d/99-vm-zram-parameters.conf
+vm.swappiness = 180
+vm.watermark_boost_factor = 0
+vm.watermark_scale_factor = 125
+vm.page-cluster = 0
+EOT
+
+bootctl list
+
+groupadd nordvpn
+groupadd libvirt
+
+useradd -m -G nordvpn,libvirt,wheel $userName
+
+echo "@includedir /etc/sudoers.d" >> /etc/sudoers
+echo "$userName ALL=(ALL) ALL" >> /etc/sudoers.d/00_$userName
+chmod 440 /etc/sudoers.d/*
+echo "User $userName has been added to sudoers."
+
+# Set root password
+echo "Setting root password..."
+
+# Set new user's password
+echo "Setting password for new user $userName..."
+ 
+chpasswd <<EOT
+$userName:$USERPASS
+root:$ROOTPASS
+EOT
+
+gpasswd -a $userName nordvpn
+
+systemctl enable NetworkManager.service
+systemctl enable sshd.service
+systemctl enable libvirtd.service
+
+EOF
---
+++ partitionDisk2Gpt.sh
@@ -0,0 +1,136 @@
+#!/bin/env bash
+
+# Function to append "M" to a variable
+addM() {
+    local var="$1"
+    local size="${!var}M"  # Append "M" to the value
+    eval "$var=\$size"  # Update the variable with the new value
+}
+
+# Show available disks on the computer
+showDisk() {
+    echo "--- YOUR DISKS ---"
+    echo
+    sudo fdisk -l | grep "Disk" | grep "/dev/"
+    chooseDisk
+}
+
+# Prompt the user to enter the disk name
+chooseDisk() {
+    echo
+    echo "--- ENTER YOUR DISK NAME (e.g., sda, nvme0n1) ---"
+    echo
+    read -r diskName
+    echo
+    
+    checkDisk
+}
+
+# Check if the entered disk name is valid
+checkDisk() {
+    local disk_output
+    disk_output=$(fdisk -l | grep "Disk" | grep "/dev/" | awk '{ print $2 }' | sed 's|/dev/||; s|:||')
+
+    # Convert the output to an array
+    IFS=$'\n' read -r -d '' -a disk_array <<< "$disk_output"
+
+    # Loop through the array
+    for disk in "${disk_array[@]}"; do
+        if [ "$disk" = "$diskName" ]; then
+            echo "-- MATCH FOUND: $disk --"
+            # Get the disk size
+            diskSize=$(lsblk -b | grep -w "$diskName" | awk '{ print $4 }')
+            diskSize=$(echo "scale=2; $diskSize / (1024^3)" | bc)  # Convert size from bytes to gigabytes
+            part
+            return
+        fi    
+    done
+
+    echo "-- NOT A VALID DISK! TRY AGAIN --"
+    showDisk
+}
+
+# Function to check if a string is an integer
+is_integer() {
+    [[ $1 =~ ^[0-9]+$ ]]
+}
+
+# Check if the number is valid
+checkNumber() {
+    if [ -z "$1" ]; then
+        echo "-- EMPTY ENTRIES ARE NOT ALLOWED --"
+        $2
+    elif ! is_integer "$1"; then
+        echo "-- ONLY INTEGERS ARE ALLOWED --"
+        $2
+    elif [ "$1" -eq 0 ]; then
+        echo "-- 0 IS NOT A VALID ENTRY --"
+        $2
+    else
+        echo "-- OK LET'S KEEP GOING --"
+    fi
+}
+
+# Partition the disk based on user input
+part() {
+    echo "--- DISK $diskName IS $diskSize GB ---"
+    echo
+    echo "-- How many OS would you like to install? --"
+    echo
+    read -r osNumber
+    checkNumber "$osNumber" part
+    bootSize1=$((550)) # Boot size in MB
+    bootSize=$(echo "$osNumber * $bootSize1" | bc) # Total boot size in MB
+    echo
+    echo "-- BOOT SIZE WILL BE $bootSize MB --"
+    echo
+    echo "-- CHOOSE SIZE FOR THE MAIN OS IN GIGABYTES --"
+    read -r mainOsSize
+    checkNumber "$mainOsSize" part
+    mainOsMb=$(echo "$mainOsSize * 1024" | bc) # Convert to MB
+    restOsSize=$(echo "($diskSize * 1024) - $mainOsMb - $bootSize" | bc) # Remaining size in MB
+    echo
+    echo "-- STILL $restOsSize MB TO BE USED --"
+    echo
+    partitionSize
+}
+
+# Calculate partition sizes
+partitionSize() {
+    restOsNumber=$((osNumber - 1))
+    partSize=$(echo "scale=2; ($restOsSize / $restOsNumber) - 1" | bc) # Size of each remaining partition in MB
+    totalPartitionNumber=$((osNumber + 1))
+    echo
+    echo "-- $totalPartitionNumber PARTITIONS WILL BE MADE --"
+    echo
+    echo "-- 1 BOOT on '${diskName}1' WILL BE + $bootSize MB --"
+    echo "-- 1 MAIN OS on '${diskName}2' WILL BE + $mainOsSize GB --"
+    echo "-- $restOsNumber OTHER PARTITIONS OF + $partSize MB WILL BE MADE --"
+    echo
+    addM bootSize
+    addM mainOsMb
+    addM partSize
+    buildPart
+}
+
+# Build partition command sequence
+buildPart() {
+    gdisk_partition_procedure=("o" "y" "n" " " " " "+$bootSize" "ef00" "n" " " " " "+$mainOsMb" "8300")
+    restOsNumber=$((osNumber - 1))
+    for ((i = 1; i <= restOsNumber; i++)); do
+        gdisk_partition_procedure+=("n" " " " " "+$partSize" "8300")
+    done
+    endArray=("w" "y")
+    gdisk_partition_procedure+=("${endArray[@]}")
+    jobDone
+}
+
+# Execute the partitioning
+jobDone() {
+    gdisk /dev/$diskName <<EOF
+$(printf "%s\n" "${gdisk_partition_procedure[@]}")
+EOF
+}
+
+# Start the script by showing disks
+#showDisk
